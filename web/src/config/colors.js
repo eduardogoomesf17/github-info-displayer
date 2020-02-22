cd9170b5b098b1b0cf3dbe3101825666794147fc
Eduardo Gomes <edwardtjs@gmail.com>
2020-02-22 14:33:03 -0300

    Committing colors config file

---
+++ web/src/config/colors.js
@@ -0,0 +1,6 @@
+export const TEXT_COLOR = '#FFF';
+export const HOVER_COLOR = '#2ecc71';
+export const BORDER_COLOR = '#3498db';
+export const BACKGROUND_COLOR = '#34495e';
+export const CONTAINER_BACKGROUND_COLOR = '#000';
+export const DEFAULT_BACKGROUND_COLOR = '#000';
